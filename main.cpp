e50b5f7b88dbe84808175498870451640260743f
yashraj <akhil@apralabs.com>
2021-02-15 19:14:47 +0530

    added boost::chrono files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+//#include<iostream>
+//int main()
+//{
+//	std::cout<<"Building";
+//	std::cout<<"Build Complete";
+//	std::cout<<"final verification one";
+//	return 0;
+//}
+
+
+
+#include <boost/chrono.hpp>
+#include <cmath>
+#include<bits/stdc++.h>
 int main()
 {
-	std::cout<<"Building";
-	std::cout<<"Build Complete";
-	std::cout<<"final verification one";
-	return 0;
+    boost::chrono::system_clock::time_point start = boost::chrono::system_clock::now();
+
+    for ( long i = 0; i < 10000000; ++i )
+    std::sqrt( 123.456L ); // burn some time
+
+    boost::chrono::duration<double> sec = boost::chrono::system_clock::now() - start;
+    std::cout << "took " << sec.count() << " seconds\n";
+    return 0;
 }
